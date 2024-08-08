cdff612c2a8c78c46a04e1082f3c5ccc398cba3f
basilmnazar <basilmnazar@gmail.com>
2024-08-08 17:10:27 +0530

    updated

--- admin_backend/urls.py
+++ admin_backend/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import *
+from deal_app.views import*
 
 urlpatterns = [
     path('super_admin_login',super_admin_login,name='super_admin_login'),
     path('super_admin_logout',super_admin_logout,name='super_admin_logout'),
     path('outlet_list',outlet_list,name='outlet_list'),
-    path('outlet_items_edit',outlet_items_edit,name='outlet_items_edit'),
+    path('outlet_items_edit/<int:pk>/',outlet_items_edit,name='outlet_items_edit'),
+    path('outlet_items_delete/<int:pk>/',outlet_items_delete,name='outlet_items_delete'),
+
     
 
 ]
--- admin_backend/views.py
+++ admin_backend/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login,logout
 from deal_app.models import*
+from django.shortcuts import render, get_object_or_404
+
 
 
 # Create your views here.
@@ -38,5 +40,44 @@ def outlet_list(request):
     items = Outlet.objects.all()
     return render(request,'outlet_list.html',{'items':items} )
 
-def outlet_items_edit(request):
-    return render(request,'outlet_items_edit.html')
+
+def outlet_items_edit(request, pk):
+    outlet_items = get_object_or_404(Outlet, pk=pk)
+
+    if request.method == 'POST':
+        name = request.POST.get('item-name')
+        description = request.POST.get('description')
+        price = request.POST.get('item-price')
+        time_from = request.POST.get('time-from')
+        time_to = request.POST.get('time-to')
+        about = request.POST.get('about')
+        image = request.FILES.get('image')
+
+        # Update fields
+        outlet_items.item_name = name
+        outlet_items.description = description
+        outlet_items.item_price = price
+        outlet_items.start_time = time_from
+        outlet_items.end_time = time_to
+        outlet_items.about = about
+        
+        if image:
+            outlet_items.item_img = image
+        
+        outlet_items.save()
+        return redirect('outlet_list')
+
+    return render(request, 'outlet_items_edit.html', {'edit_items': outlet_items})
+
+
+def outlet_items_delete(request, pk):
+    outlet_item = get_object_or_404(Outlet, pk=pk)
+
+    if request.method == 'POST':
+        outlet_item.delete()
+        return redirect('outlet_list')
+    else:
+        # Typically, a GET request to this endpoint should be handled differently,
+        # but in this case, we just redirect to the outlet_list without deleting anything.
+        return redirect('outlet_list')
+
