04c81036288a715666f2198034949574fea2e4f4
Hudels Dev <117001322+hudelsdev@users.noreply.github.com>
2024-09-25 19:57:38 +0530

    Update models.py

--- nearbuy_app/models.py
+++ nearbuy_app/models.py
@@ -12,9 +12,3 @@ class register_user(models.Model):
     state=models.CharField(null=True,max_length=30)
     city=models.CharField(null=True,max_length=40)
     phone=models.IntegerField(null=True)
-
-class Users(models.Model):
-    name_user = models.CharField(max_length=100)
-    user_phone_number = models.CharField(max_length=15)  # Adjust length based on your needs
-    def __str__(self):
-        return f"{self.name_user} ({self.user_phone_number})"
