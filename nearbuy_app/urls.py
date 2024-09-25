315435f6643f3032888f06706f97f5b42cb90ef0
Hudels Dev <117001322+hudelsdev@users.noreply.github.com>
2024-09-25 19:58:11 +0530

    Update urls.py

--- nearbuy_app/urls.py
+++ nearbuy_app/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns =[
     
-    path('admin_register/',admin_register,name='admin_register'),
-    path('admin_login/',admin_login,name='admin_login'),
+    # path('admin_register/',admin_register,name='admin_register'),
+    # path('admin_login/',admin_login,name='admin_login'),
     path('admin_index/',admin_index,name='admin_index'),
     path('forgot_password/',forgot_password,name='forgot_password'),
     path('logout/',admin_logout, name="admin_logout"), 
